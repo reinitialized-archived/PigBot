47ada48725f765b106f8320739594856a45e4211
DBReinitialized <dbreinitialized@dbstudios.xyz>
2017-04-05 03:17:18 -0500

    PigBotServer initial publication

---
+++ Source/PigBotServer.lua
@@ -0,0 +1,14 @@
+--[[
+
+    PigBot Services
+  PigBotServer ver 0.1.0
+  Main component responsible for setting up and initializing PigBot
+
+  Change Log:
+    [452017a]
+      - Initial publication
+
+
+Help us improve PigBot Services! Contribute to our source at our public GitHub:
+https://github.com/DBReinitialized/PigBot
+]]
