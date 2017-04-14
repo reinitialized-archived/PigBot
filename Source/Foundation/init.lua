46cb2c5feefb0298e976967bec764d6a144b4ad1
DBReinitialized <dbreinitialized@dbstudios.xyz>
2017-04-14 04:16:41 -0500

    Removed previous PigBot source
    Removed previous PigBot source to begin work on Dream Foundation, the
    framework of PigBot.

---
+++ Source/Foundation/init.lua
@@ -0,0 +1,12 @@
+--[[
+
+    Dream Foundation
+    init.lua ver 0.1.0 indev
+    Main script for Dream Foundations.
+
+
+    Changelog:
+      [4142017a]
+        - Removed previous PigBot source to begin work
+        on Dream Foundation, the framework of PigBot.
+]]
